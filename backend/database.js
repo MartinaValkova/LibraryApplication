f49489a53413dcf8d5796c02ec21d66a2b36fd7f
MartinaValkova <Martinavalkova16@hotmail.com>
2024-05-06 16:28:09 +0100

    Final modifications

--- backend/database.js
+++ backend/database.js
@@ -2,6 +2,7 @@
 const { Pool } = require('pg');
 require('dotenv').config(); // Load environment variables from .env file
 
+//Database configuration
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL
 });
